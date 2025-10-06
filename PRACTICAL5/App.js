c65d0e19e0ca80ad52180c42645ebc213c1ef8b6
ShreyaGarasia <23dcs030@charusat.edu.in>
2025-10-06 15:12:40 +0530

    Add files via upload

---
+++ PRACTICAL5/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './pra5/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
