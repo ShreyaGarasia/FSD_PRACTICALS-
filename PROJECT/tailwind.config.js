28a29fb4d214b872daaa0904f8173eabfc9ba9dc
ShreyaGarasia <23dcs030@charusat.edu.in>
2025-10-07 09:41:50 +0530

    Add files via upload

---
+++ PROJECT/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust based on your file structure
+    './index.css', // Include your CSS file if it contains Tailwind directives
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
